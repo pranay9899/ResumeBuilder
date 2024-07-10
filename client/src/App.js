10809ac563d05ed89b5b6ab3671f413e15e0c336
Pranay Goud Nyalapatla <132379511+pranay9899@users.noreply.github.com>
2024-07-10 22:11:46 +0530

    Added Files
    Updated Routing for pages and its just added files and sample code to see

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import MyResume from './pages/MyResume';
+import ResumeTemplates from './pages/ResumeTemplates';
 import Users from './Users';
 
 const App = () => {
     return (
         <div>
-            <h1>Welcome</h1>
-            <Users/>
+            <BrowserRouter>
+            <Routes>
+                <Route index element={<Home/>}/>
+                <Route path="/home" element={<Home/>} />
+                <Route path="/login" element={<Login/>} />
+                <Route path="/signup" element={<Signup/>} />
+                <Route path="/profile" element={<Profile/>} />
+                <Route path="/resumetemplates" element={<ResumeTemplates/>} />
+                <Route path="/myresume" element={<MyResume/>} />
+                <Route path="/users" element={<Users/>} />
+            </Routes>
+            </BrowserRouter>
         </div>
     );
 };
---
+++ client/src/pages/MyResume.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MyResume() {
+  return <div>MyResume</div>;
+}
